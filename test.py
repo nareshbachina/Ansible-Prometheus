fd509a45af343f595a1d0b33e756ff4d8f42455f
Vinodh Kumar Basavani <vinodh2308@gmail.com>
2018-10-13 01:21:28 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 environment_variables = os.environ
-
-for key,value in environment_variables :
+print(type(environment_variables))
+for key,value in environment_variables:
   if key.startswith('GIT'):
     print(key)
     print(value)
