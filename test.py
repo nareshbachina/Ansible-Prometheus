f6d9ff6ca1c75a5402ec64079b41da353be70031
Vinodh Kumar Basavani <vinodh2308@gmail.com>
2018-10-13 01:27:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import os
 import json
 environment_variables = os.environ
-
+number_of_repositories=0
 for environment_variable in environment_variables:
-  print(environment_variable)
-print(environment_variables['GIT_URL'])
+  if environment_variable.startswith('GIT_URL'):
+    number_of_repositories+=1
+print(number_of_repositories)
