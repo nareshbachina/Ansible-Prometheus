6497ccd4077acd01ef1e9ff91377fd5ddc36c685
Vinodh Kumar Basavani <vinodh2308@gmail.com>
2018-10-13 01:29:23 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,8 @@ for environment_variable in environment_variables:
   if environment_variable.startswith('GIT_URL'):
     number_of_repositories+=1
 print(number_of_repositories)
+initial=""
+for i in range(0,number_of_repositories):
+  print(environment_variables['GIT_URL'+str(initial)])
+  initial=i
+ 
