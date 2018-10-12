1177a09dd9bd863241de5f3d1bcab4da66bac821
Vinodh Kumar Basavani <vinodh2308@gmail.com>
2018-10-13 01:25:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import os
 import json
-environment_variables = json.loads(str(os.environ))
-print(type(environment_variables))
-for key,value in environment_variables:
-  if key.startswith('GIT'):
-    print(key)
-    print(value)
+environment_variables = os.environ
+
+for environment_variable in environment_variables:
+  print(environment_variable)
